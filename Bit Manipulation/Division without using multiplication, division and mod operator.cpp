90f41d3c77842994e30b4a0bf1f538b44c7ded31
Cherian John <109579008+CjBattle@users.noreply.github.com>
2023-03-13 00:13:44 +0530

    Add files via upload

---
+++ Bit Manipulation/Division without using multiplication, division and mod operator.cpp
@@ -0,0 +1,26 @@
+class Solution
+{
+public:
+    long long divide(long long dividend, long long divisor)
+    {
+        // code here
+        int sign = ((dividend < 0) ^ (divisor < 0)) ? -1 : 1;
+
+        dividend = abs(dividend);
+        divisor = abs(divisor);
+        int long long quotient = 0, temp = 0;
+
+        for (int i = 31; i >= 0; --i)
+        {
+            if (temp + (divisor << i) <= dividend)
+            {
+                temp += divisor << i;
+                quotient += 1LL << i;
+            }
+        }
+        if (sign == -1)
+            quotient = -quotient;
+
+        return quotient;
+    }
+};
