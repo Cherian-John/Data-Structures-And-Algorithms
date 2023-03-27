309577d7e6d5a02b65af031fb8564571f77a3c64
Cherian John <109579008+CjBattle@users.noreply.github.com>
2023-03-27 14:10:38 +0530

    Add files via upload

---
+++ Sorting/bubblesort.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+public:
+    // Function to sort the array using bubble sort algorithm.
+    void bubbleSort(int arr[], int n)
+    {
+        // Your code here
+        for (int i = n - 1; i >= 0; i--)
+        {
+            for (int j = 0; j <= i - 1; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    int temp = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
+};
